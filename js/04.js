db9960a086b549443e5fbbee6ec4f574f07f52e6
sayanishioka <n_saya@hotmail.co.jp>
2015-11-26 11:21:05 +0900

    中間テスト全クリ！！！！！

---
+++ js/04.js
@@ -0,0 +1,7 @@
+function buzz(a){
+
+  if(a % 5 == 0){
+    return "buzz";
+  }
+  return a
+}
---
+++ js/05.js
@@ -0,0 +1,12 @@
+function fizzbuzz(a){
+  if(a % 3 == 0 && a % 5 == 0){
+    return "fizzbuzz";
+  } else if(a % 3 == 0){
+    return "fizz";
+
+  } else if(a % 5 == 0){
+    return "buzz";
+
+  }
+  return a;
+}
